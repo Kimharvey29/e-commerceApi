febf3a3db68472e3d2f6335b117b8c407786ecce
Kimharvey.esteron <kimharveye@gmail.com>
2025-07-26 16:02:02 +0800

    Second commit

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(express.urlencoded({extended: true}));
 // costumize the CORS option to meet your specific requirements:
 
 const corsOptions = {
-	origin: ['http://localhost:3000'],
+	origin: ['http://localhost:3000', 'https://e-commerceapi-3.onrender.com'],
 	// methods: ['GET'] //allow only specified HTTP methods //optional only if you want to restrict methods
 	// allowHeaders: ['Content-Type', "Authorization"], //allow specified
 	credentials: true, //allow credentials example cookis, authorization headers
